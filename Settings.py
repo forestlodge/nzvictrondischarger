31c42fc4b7978ab151bcf2db7ff336dab39c26b5
Mike Casey <mikecasey@mikes-mbp-2.lan>
2021-03-08 10:24:29 +1300

    first update to create a settings.py file

---
+++ Settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+SETTINGS.py
+"""
+
+SYSTEM_IP="192.168.xx.xx" #the internal IP Address of the GX Unit Settings > Ethernet > IP Address
+SYSTEM_PORT=502 #default Victron port
+
+EA_SUBSCRIPTION_KEY = '' #e.g s33e065901b8207a9ae56901ba16bf7f you need to register with the New Zealand Electricity Authority to 
+#be given a key. Good news is they are encouraging projects like this to use there API so let them know exactly what you are doing.
+#Register for the wholesale market prices API here: emi.portal.azure-api.net
